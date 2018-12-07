a563c9e69ce2ab6fc61a3741a90b8ae154d6dba7
Masood Alam <masoodalam.hunzai@gmail.com>
2018-12-07 16:42:09 +0500

    changes in js

--- app.js
+++ app.js
@@ -3,54 +3,55 @@
 //  "" + 1 + 0
 
 var a = "" + 1 + 0;
- console.log(a);  // Result 10
+ console.log('"" + 1 + 0 = ' +a);  // Result 10
 
+ //"" - 1 + 0
 var b = "" - 1 + 0;
- console.log(b);  // Result -1
+ console.log('"" - 1 + 0 = '+b);  // Result -1
 
  //true + false
   var c = true + false;
- console.log(c); // 1
+ console.log('true + false = '+c); // 1
 
  //6 / "3"
   var c = 6 / "3";
- console.log(c); // 2
+ console.log('6 / "3"= '+c); // 2
 
  //"2" * "3"
   var d = "2" * "3";
- console.log(d); //  6
+ console.log('"2" * "3" = ' +d); //  6
 
   //4 + 5 + "px"
   var e = 4 + 5 + "px"
- console.log(e); //  9px
+ console.log('4 + 5 + "px" = '+e); //  9px
 
   //"$" + 4 + 5"
   var f = "$" + 4 + 5
- console.log(f); //  $45
+ console.log('"$" + 4 + 5" = '+f); //  $45
 
   //"4" - 2
   var g = "4" - 2
- console.log(g); //  2
+ console.log('"4" - 2 = '+g); //  2
 
   //. 7 / 0
   var h = 7 / 0
- console.log(h); //  Infinity
+ console.log('7 / 0 = '+h); //  Infinity
 
   //. " -9\n" + 5
   var i = " -9\n" + 5
- console.log(i); //   -9
+ console.log(' " -9\n" + 5 = '+i); //   -9
 
   //. " -9\n" - 5
-  var i = " -9\n" - 5
- console.log(i); //   -14
+  var j = " -9\n" - 5
+ console.log(' " -9\n" - 5 = '+j); //   -14
 
   //. null + 1
-  var i = null + 1
- console.log(i); //   1
+  var k = null + 1
+ console.log('null + 1 = '+k); //   1
 
   //. undefined + 1
-  var i = undefined + 1
- console.log(i); //   NaN
+  var m = undefined + 1
+ console.log('undefined + 1 = '+m); //   NaN
 
 
 
