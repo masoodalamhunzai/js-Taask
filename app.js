4933f30a2d94c9cc69d0600587b9584bfdc98bdd
Masood Alam <masoodalam.hunzai@gmail.com>
2018-12-07 16:14:03 +0500

    added js task

---
+++ app.js
@@ -0,0 +1,56 @@
+// OutPut
+
+//  "" + 1 + 0
+
+var a = "" + 1 + 0;
+ console.log(a);  // Result 10
+
+var b = "" - 1 + 0;
+ console.log(b);  // Result -1
+
+ //true + false
+  var c = true + false;
+ console.log(c); // 1
+
+ //6 / "3"
+  var c = 6 / "3";
+ console.log(c); // 2
+
+ //"2" * "3"
+  var d = "2" * "3";
+ console.log(d); //  6
+
+  //4 + 5 + "px"
+  var e = 4 + 5 + "px"
+ console.log(e); //  9px
+
+  //"$" + 4 + 5"
+  var f = "$" + 4 + 5
+ console.log(f); //  $45
+
+  //"4" - 2
+  var g = "4" - 2
+ console.log(g); //  2
+
+  //. 7 / 0
+  var h = 7 / 0
+ console.log(h); //  Infinity
+
+  //. " -9\n" + 5
+  var i = " -9\n" + 5
+ console.log(i); //   -9
+
+  //. " -9\n" - 5
+  var i = " -9\n" - 5
+ console.log(i); //   -14
+
+  //. null + 1
+  var i = null + 1
+ console.log(i); //   1
+
+  //. undefined + 1
+  var i = undefined + 1
+ console.log(i); //   NaN
+
+
+
